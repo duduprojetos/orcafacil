05e1d71b5d9a4f96b601866c7614efebb8c4048f
Eduardo <dudu483610@gmail.com>
2026-08-19 14:55:05 -0300

    config: desabilitar cache do navegador

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  headers: async () => {
+    return [
+      {
+        // Aplica em TODAS as rotas
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
